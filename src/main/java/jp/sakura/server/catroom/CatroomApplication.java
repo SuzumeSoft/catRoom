26f5aaa39c30b31e991e6ea562b0ae16ad1d2df7
rei <reitwlm1007@gmail.com>
2017-06-18 20:21:01 +0900

    first commit

---
+++ src/main/java/jp/sakura/server/catroom/CatroomApplication.java
@@ -0,0 +1,12 @@
+package jp.sakura.server.catroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatroomApplication.class, args);
+	}
+}
---
+++ src/test/java/jp/sakura/server/catroom/CatroomApplicationTests.java
@@ -0,0 +1,16 @@
+package jp.sakura.server.catroom;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CatroomApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
